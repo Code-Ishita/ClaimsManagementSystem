5d7ee29b07f8f5cece2f4c68368cdbd4cc410a9c
Code-Ishita <mailtoishitakumari@gmail.com>
2022-09-15 14:29:52 +0530

    first commit

---
+++ ClaimManagement_Backend/MFPE_policyService/src/main/java/com/policy/microservice/feign/AuthClient.java
@@ -0,0 +1,17 @@
+package com.policy.microservice.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import com.policy.microservice.dto.ValidatingDTO;
+
+
+//@FeignClient(name = "auth-client", url = "${Authorization.URL}")
+@FeignClient(name="AUTHORIZATION-SERVICE/authorization")
+public interface AuthClient {
+	
+	 @GetMapping(value = "/validate")
+     public ValidatingDTO getsValidity(@RequestHeader(name = "Authorization", required = true) String token);
+
+}
